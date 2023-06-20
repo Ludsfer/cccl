5a8bb54c729a6a224c86f69549f89b36960d0ce6
Michael Schellenberger Costa <miscco@nvidia.com>
2023-06-20 11:26:01 +0200

    Move `terminate` into its own file

---
+++ libcudacxx/include/cuda/std/detail/libcxx/include/__exception/terminate.h
@@ -0,0 +1,33 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+// Part of libcu++, the C++ Standard Library for your entire system,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _LIBCUDACXX___EXCEPTION_TERMINATE_H
+#define _LIBCUDACXX___EXCEPTION_TERMINATE_H
+
+#ifndef __cuda_std__
+#include <__config>
+#endif //__cuda_std__
+
+#if defined(_CCCL_IMPLICIT_SYSTEM_HEADER_GCC)
+#  pragma GCC system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_CLANG)
+#  pragma clang system_header
+#elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
+#  pragma system_header
+#endif // no system header
+
+_LIBCUDACXX_BEGIN_NAMESPACE_STD_NOVERSION // purposefully not using versioning namespace
+
+_LIBCUDACXX_NORETURN _LIBCUDACXX_FUNC_VIS void terminate() noexcept;
+
+_LIBCUDACXX_END_NAMESPACE_STD_NOVERSION
+
+#endif // _LIBCUDACXX___EXCEPTION_TERMINATE_H
