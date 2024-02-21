56b85b96e9a0b6bfceefc8a5dc7c39e12b49c022
Michael Schellenberger Costa <miscco@nvidia.com>
2024-02-21 09:51:36 +0100

    Implement `ranges::iota_view`

---
+++ libcudacxx/test/libcudacxx/std/ranges/range.factories/range.iota.view/sentinel/ctor.default.pass.cpp
@@ -0,0 +1,35 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of libcu++, the C++ Standard Library for your entire system,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+// UNSUPPORTED: msvc-19.16
+
+// sentinel() = default;
+
+#include <cuda/std/ranges>
+#include <cuda/std/cassert>
+
+#include "test_macros.h"
+#include "../types.h"
+
+__host__ __device__ constexpr bool test() {
+  using Sent = cuda::std::ranges::sentinel_t<cuda::std::ranges::iota_view<Int42<DefaultTo42>, IntComparableWith<Int42<DefaultTo42>>>>;
+  using Iter = cuda::std::ranges::iterator_t<cuda::std::ranges::iota_view<Int42<DefaultTo42>, IntComparableWith<Int42<DefaultTo42>>>>;
+  assert(Sent() == Iter());
+
+  return true;
+}
+
+int main(int, char**) {
+  test();
+  static_assert(test(), "");
+
+  return 0;
+}
---
+++ libcudacxx/test/libcudacxx/std/ranges/range.factories/range.iota.view/sentinel/ctor.value.pass.cpp
@@ -0,0 +1,49 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of libcu++, the C++ Standard Library for your entire system,
+// under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14
+// UNSUPPORTED: msvc-19.16
+
+// constexpr explicit sentinel(Bound bound);
+
+#include <cuda/std/ranges>
+#include <cuda/std/cassert>
+
+#include "test_macros.h"
+#include "../types.h"
+
+__host__ __device__ constexpr bool test() {
+  {
+    using Sent = cuda::std::ranges::sentinel_t<cuda::std::ranges::iota_view<int, IntSentinelWith<int>>>;
+    using Iter = cuda::std::ranges::iterator_t<cuda::std::ranges::iota_view<int, IntSentinelWith<int>>>;
+    auto sent = Sent(IntSentinelWith<int>(42));
+    assert(sent == Iter(42));
+  }
+  {
+    using Sent = cuda::std::ranges::sentinel_t<cuda::std::ranges::iota_view<SomeInt, IntSentinelWith<SomeInt>>>;
+    using Iter = cuda::std::ranges::iterator_t<cuda::std::ranges::iota_view<SomeInt, IntSentinelWith<SomeInt>>>;
+    auto sent = Sent(IntSentinelWith<SomeInt>(SomeInt(42)));
+    assert(sent == Iter(SomeInt(42)));
+  }
+  {
+    using Sent = cuda::std::ranges::sentinel_t<cuda::std::ranges::iota_view<SomeInt, IntSentinelWith<SomeInt>>>;
+    static_assert(!cuda::std::is_convertible_v<Sent, IntSentinelWith<SomeInt>>);
+    static_assert( cuda::std::is_constructible_v<Sent, IntSentinelWith<SomeInt>>);
+  }
+
+  return true;
+}
+
+int main(int, char**) {
+  test();
+  static_assert(test(), "");
+
+  return 0;
+}
